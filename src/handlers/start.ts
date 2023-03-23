ee17c29a905b5e46fd817c627efa6a49682d88b0
coalus <nomail@gmail.com>
2023-03-23 18:36:18 +0000

    fix: fix displaying of last name

--- src/handlers/start.ts
+++ src/handlers/start.ts
@@ -1,8 +1,10 @@
 import MyContext from '@/models/Context'
 
 export async function handleStart(ctx: MyContext) {
+  const lastName = ctx.message?.from?.last_name
   const userName =
-    ctx.message?.from?.first_name + ' ' + ctx.message?.from?.last_name ? ctx.message?.from?.last_name : ''
+    ctx.message?.from?.first_name + ' ' + (lastName ? lastName : '')
+      
   const text = `Hi, ${userName}
 Welcome to CapyCloud, a decentralized and convenient solution for communication with the Ton Storage. 
 
