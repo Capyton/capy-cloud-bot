6df0ca48022d50c8607919d6855935be8ff2f153
SamWarden <SamWardenSad@gmail.com>
2023-03-27 02:33:36 +0300

    Add `AuthTokens` entity

---
+++ src/entities/auth-tokens.ts
@@ -0,0 +1,11 @@
+import { UUID } from "@src/utils/uuid"
+
+export class AuthTokens {
+    constructor(
+        readonly id: UUID,
+        readonly accessToken: string,
+        readonly refreshToken: string,
+        readonly tgUserId: UUID,
+        readonly tonAddress: string | null = null,
+    ) { }
+}
